5a3d77238477057758615d7d36fd241f2f371d1a
tamaratsymbaliuk <tsymbalt14@gmail.com>
2025-02-09 19:48:00 -0600

    final

---
+++ .next/static/chunks/src_app_auth_sign-in_page_tsx_941a0a._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_auth_sign-in_page_tsx_941a0a._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_auth_sign-in_page_tsx_941a0a._.js",
+  "chunks": [
+    "static/chunks/src_app_auth_sign-in_page_tsx_2db5df._.js"
+  ],
+  "source": "dynamic"
+});
