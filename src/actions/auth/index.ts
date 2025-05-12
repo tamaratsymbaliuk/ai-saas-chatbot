5fa0e4e96929ab68e54b36b6b1bd121e9f390d47
tamaratsymbaliuk <tsymbalt14@gmail.com>
2025-05-11 19:37:38 -0500

    updated mvp compelte

--- src/actions/auth/index.ts
+++ src/actions/auth/index.ts
@@ -1,2 +1,60 @@
 'use server'
 
+import { client } from '@/lib/prisma'
+import { currentUser } from '@clerk/nextjs/server'
+import { onGetAllAccountDomains } from '../settings/index'
+
+export const onCompleteUserRegistration = async (
+  fullname: string,
+  clerkId: string,
+  type: string
+) => {
+  try {
+    const registered = await client.user.create({
+      data: {
+        fullname,
+        clerkId,
+        type,
+        subscription: {
+          create: {},
+        },
+      },
+      select: {
+        fullname: true,
+        id: true,
+        type: true,
+      },
+    })
+
+    if (registered) {
+      return { status: 200, user: registered }
+    }
+  } catch (error) {
+    return { status: 400, error }
+  }
+}
+
+export const onLoginUser = async () => {
+  const user = await currentUser()
+  if (!user) return { status: 400, error: 'User not found' }
+  else {
+    try {
+      const authenticated = await client.user.findUnique({
+        where: {
+          clerkId: user.id,
+        },
+        select: {
+          fullname: true,
+          id: true,
+          type: true,
+        },
+      })
+      if (authenticated) {
+        const domains = await onGetAllAccountDomains()
+        return { status: 200, user: authenticated, domain: domains?.domains }
+      }
+    } catch (error) {
+      return { status: 400, error }
+    }
+  }
+}
